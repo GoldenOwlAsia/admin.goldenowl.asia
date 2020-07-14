7e2736550b09102ea63dc23a88d368caa06c2d3e
liam-le-goldenowl <liam.le.goldenowl@gmail.com>
2020-07-14 10:25:45 +0700

    refactor code on sent mail

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -2,19 +2,17 @@ class SubscriptionMailer < ApplicationMailer
   default from: 'austin@goldenowl.asia'
 
   def subscription_email_for_post(email)
-    lastest_post_url = post_url(Post.last)
     mail(to: email, subject: 'Subscription Email') do |format|
       format.html {
-        render locals: { url: lastest_post_url }
+        render locals: { url: post_url(Post.last) }
       }
     end
   end
 
   def subscription_email_for_career(email)
-    lastest_carrer_url = career_url(Career.last)
     mail(to: email, subject: 'Subscription Email') do |format|
       format.html {
-        render locals: { url: lastest_carrer_url }
+        render locals: { url: career_url(Career.last) }
       }
     end
   end
