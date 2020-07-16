dbdb11610a07cfa92d14e37ba32bbd1ae025ce4f
liam-le-goldenowl <liam.le.goldenowl@gmail.com>
2020-07-16 14:07:05 +0700

    --amend--no-edit

--- db/migrate/20200716065118_generate_slugs_for_existing_posts_and_careers.rb
+++ db/migrate/20200716065118_generate_slugs_for_existing_posts_and_careers.rb
@@ -1,6 +1,6 @@
 class GenerateSlugsForExistingPostsAndCareers < ActiveRecord::Migration[6.0]
   def change
     Post.find_each(&:save)
-    Carere.find_each(&:save)
+    Career.find_each(&:save)
   end
 end
